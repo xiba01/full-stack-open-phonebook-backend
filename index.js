4eab0f1fac7faf476d4947b0ea365d6763370ba1
Mostafa Chiba <chibamostafa.real@gmail.com>
2024-09-29 10:36:37 +0100

    3.9 exercice

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("body", (req) => {
@@ -11,10 +12,11 @@ morgan.token("body", (req) => {
 });
 
 app.use(express.json());
-app.use(morgan("tiny"));
-// app.use(
-//   morgan(":method :url :status :res[content-length] - :response-time ms :body")
-// );
+// app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+app.use(cors());
 
 let persons = [
   {
